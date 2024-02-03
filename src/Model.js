98d42624260ff96eb8d003989e8fbee98f611200
Abhishek kumar <aasalex21c@gmail.com>
2024-02-03 14:51:43 +0530

    working on it

--- src/Model.js
+++ src/Model.js
@@ -1,9 +1,53 @@
 'use strict';
+
+
+
 class Model{
   constructor(){    
+    this.fetchedAPIData = {
+      bitCoin: 0,
+      ethereum: 0,
+      BinanceCoin: 0
+    };
     
-   
+    // console.log(this.fetchCryptoData());
+      // this.requestDataEveryNSeconds();
   }  
+
+  async requestDataEveryNSeconds(nSeconds=15000){
+    // initially calling once
+    this.fetchCryptoData();
+    setInterval(()=>{
+      this.fetchCryptoData();
+    },nSeconds);
+  }
+
+  async fetchCryptoData() {
+    const timestamp = new Date().getTime(); // Generate a timestamp to use as a cache buster
+    let url = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=bitcoin,ethereum,binancecoin&_=${timestamp}`;
+
+    try{
+      // console.log(this);
+      let response = await fetch(url);
+      let data = await response.json();
+      data.forEach((coin)=>{
+        // console.log(coin);
+        if(coin.symbol.toLowerCase() === 'btc'){
+          this.fetchedAPIData.bitCoin = coin.current_price;
+        }else if(coin.symbol.toLowerCase() === 'eth'){
+          this.fetchedAPIData.bitCoin = coin.current_price;
+        }else if(coin.symbol.toLowerCase() === 'bnb'){
+          this.fetchedAPIData.bitCoin = coin.current_price;
+        }
+      });
+
+      return true;
+    }catch(error){
+      return false;
+    }
+
+  }
+  
 
 
 
--- src/View.js
+++ src/View.js
@@ -6,14 +6,70 @@ class View{
 
   constructor(model){
     this.model = model;
-    // chart metadata   
-      this.chart = null;
-      this.ctx =  document.getElementById('myChart');      
-      this.ctx.width = 500;
-      this.ctx.height = 200;
+    // initializing charts
+      this.chartFake = null;
+      this.chartGenuineBitCoin = null;
+      this.chartGenuineEthereum = null;
+      this.chartGenuineBinanceCoin = null;
 
+
+
+      this.chartsData = {
+        chartFake: {
+          data: {
+            labels: ['0s'],
+            datasets: [{
+              label: 'Fake Data (Math.random())',
+              data: [42000],
+              borderWidth: 1,
+            }]
+          },
+          ctx: document.getElementById('chartFake'),
+          width: 500,
+          height: 200
+        },
+        chartGenuineBitCoin: {
+          data: {
+            labels: ['0s'],
+            datasets: [{
+              label: 'ETH (USD)',
+              data: [42000],
+              borderWidth: 1,
+            }]
+          },
+          ctx: document.getElementById('chartGenuineBitCoin'),
+          width: 500,
+          height: 200
+        },
+        chartGenuineEthereum: {
+          data: {
+            labels: ['0s'],
+            datasets: [{
+              label: 'ETH (USD)',
+              data: [42000],
+              borderWidth: 1,
+            }]
+          },
+          ctx : document.getElementById('chartGenuineEthereum'),
+          width: 500,
+          height: 200
+        },
+        chartGenuineBinanceCoin: {
+          data: {
+            labels: ['0s'],
+            datasets: [{
+              label: 'BNB (USD)',
+              data: [42000],
+              borderWidth: 1,
+            }]
+          },
+          ctx: document.getElementById('chartGenuineBinanceCoin'),
+          width: 500,
+          height: 200
+        }
+        
+      };
   
-
       this.colors={
         'slate700': 'rgb(51 65 85)',
         'zinc50': 'rgb(250 250 250)'
@@ -21,17 +77,8 @@ class View{
       this.stylesTable={
         border: "border-solid border-2 border-slate-700"
       };
-
-      this.chartMetadata={
-      
-        data: {
-          labels: ['0s'],
-          datasets: [{
-            label: 'Fake Data generated every 5s using Math.random()',
-            data: [42000],
-            borderWidth: 1,
-          }]
-        },
+  // chart metadata   
+      this.chartMetadataTemplate={      
         options: {
           scales: {
             x: {
@@ -69,40 +116,48 @@ class View{
 
 
         }
-      }
+      };
+      
     // canvas configure
     // this.configureCanvas();
 
 
     // event listeners and DOM Fetching
-      this.tableInvestmentData = document.querySelector('table#tableInvestmentData');
-      // console.log(this.tableInvestmentData);
-      this.selectChartType = document.querySelector('select#selectChartType');
-      this.selectChartType.addEventListener('change', ()=>{
-        // console.log(this.selectChartType.value);
-        this.updateChartType(this.selectChartType.value);        
-      });
-      window.addEventListener('resize', this.throttling(this.resizeChart, 200, this.ctx));
+      // window.addEventListener('resize', this.throttling(this.resizeChart, 200, this.ctx));
 
     // generating investment table
       // this.generateInvestmentTable();
-this.generateChart('line');
+this.generateChart(this.chartFake, this.ctxChartFake, this.chartsData.chartFake, 'line', true);
+// this.generateChart(this.chartGenuineBitCoin, this.ctxChartGenuineBitCoin, this.chartsData.chartGenuineBitCoin, 'line');
+// this.generateChart(this.chartGenuineEthereum, this.ctxChartGenuineEthereum, this.chartsData.chartGenuineEthereum, 'line');
+// this.generateChart(this.chartGenuineBinanceCoin, this.ctxChartGenuineBinanceCoin, this.chartsData.chartGenuineBinanceCoin, 'line');
+
 // this.selectChartType.classList.remove('displayNone'); 
 
- // fake data generator activate
-    setInterval(()=>{
-    this.updateFakeDataChart();
-    },5000);
+
     
   }
 
-  updateFakeDataChart(){
-    let newLabel = (Number(this.chart.data.labels.at(-1).replace('s', '')) + 5  + 's');
-    this.chart.data.labels.push(newLabel);
+  updateFakeDataChart(chart){
+    // console.log(chart.data)
+    let newLabel = (Number(chart.data.labels.at(-1).replace('s', '')) + 5  + 's');
+    chart.data.labels.push(newLabel);
     let newFakeData = (Math.random()*45000).toFixed(2);
-    this.chart.data.datasets[0].data.push(newFakeData);
+    chart.data.datasets[0].data.push(newFakeData);    
+    chart.update();
+  }
+
+  async updateGenuineChartData(chart){
+    // console.log('okay listened to you!', chart);
+    console.log(this.model.fetchedAPIData);
+    // let newLabel = (Number(chart.data.labels.at(-1).replace('s', '')) + 5  + 's');    
+    // chart.data.labels.push(newLabel);
+
+    // let newFakeData = (Math.random()*45000).toFixed(2);
+    // chart.data.datasets[0].data.push(newFakeData);    
+    // chart.update();
     
-    this.chart.update();
+    
   }
 
   throttling(callback, delay, ctx){
@@ -135,17 +190,29 @@ this.generateChart('line');
   }
 
 
-  generateChart(chartType){
-    if(this.chart){
-      this.chart.destroy();
-    }
+  generateChart(chart, ctx, chartData, chartType){
+    
+    if(chart){
+      chart.destroy();
+    }    
+    // console.log(chartData.data.labels);
     setTimeout(()=>{
-      this.chart = new Chart(this.ctx, {
+      
+      chart = new Chart(ctx, {
         type: chartType, // good one are: pie, bar, doughnut
-        data: {...this.chartMetadata.data},
-        options: {...this.chartMetadata.options}
+        data: {...chartData.data},
+        options: {...this.chartMetadataTemplate.options}
       });
-      this.resizeChart(this.ctx);
+       //activate real time data fetch       
+        setInterval(()=>{     
+          if(isItFakeChart){
+            this.updateFakeDataChart(chart);
+          }else{
+            this.updateGenuineChartData(chart);
+          }     
+        },5000);
+
+    //   this.resizeChart(ctx);
       setTimeout(()=>{
         anime({
           targets: '#myChart',
